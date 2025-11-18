0f59b99b0d96e3471d264c298d9546ade7974d14
Sidneygomes878 <sidneygomesdev@gmail.com>
2025-11-18 17:11:15 -0300

    Enviar

---
+++ modulo1/aula1.py
@@ -0,0 +1,13 @@
+print("Hello word!")
+
+Sidney = 1.75 
+Erickson = 1.70
+
+if Sidney > Erickson:
+    print("Sidney é maior que Gomes")
+
+else:
+    print("Gomes é maior que Sidney")
+
+    if Sidney == Erickson:
+        print("Sidney é igual a Gomes")
