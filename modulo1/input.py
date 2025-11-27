575e3f4cd3789a7234b63e1f0fd101f9d1c46265
Sidneygomes878 <sidneygomesdev@gmail.com>
2025-11-27 15:28:22 -0300

    input

---
+++ modulo1/input.py
@@ -0,0 +1,8 @@
+facul = input("Qual é a sua faculdade?")
+periodo = input("São quantos periodos?") 
+print("Minha faculdade é " + facul + " e são " + periodo + " periodos.")
+
+avaliacao = "Eu dou"
+continuacao = "cinco estrelas"
+print(avaliacao, end = " ")
+print(continuacao)
